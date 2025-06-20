84f83a05e43c2e207f2a8d2cf9813a7f42aa064f
abdulmazidakash <akashabdulmazid01@gmail.com>
2025-06-20 23:36:53 +0600

    footer done

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -3,10 +3,15 @@ import React from 'react'
 
 export default function Footer() {
   return (
-	<div>
-		<span>Right</span>
-		<Link href={'/'}>Abdul Mazid Akash</Link>
-		<Link href={'/'} >Say Hello</Link>
+	<div className='w-full border-t-2 border-solid border-dark font-semibold text-lg px-32 '>
+		<div className='py-8 flex items-center justify-between'>
+			<span>{new Date().getFullYear()} &copy; All Rights Reserved.</span>
+		<div className='flex items-center'>
+			Build With <span className='text-primary text-2xl px-1'>&#9825;</span>by&nbsp;
+			<Link href={'/'} className='underline underline-offset-2' target='_blank'>Abdul Mazid Akash</Link>
+		</div>
+		<Link href={'/'} className='underline underline-offset-2' target='_blank'>Say Hello</Link>
+		</div>
 	</div>
   )
 }
